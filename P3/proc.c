4ca512d5d5e46e57a6dfea2099bc728bb3e019d5
juanjogomezvillegas <juanjogomvil@gmail.com>
2024-04-04 18:11:46 +0200

    Afegit el codi que crea la jerarquia de processos per executar comandes de unix des de crides de C

--- P3/proc.c
+++ P3/proc.c
@@ -7,10 +7,12 @@
 #include <unistd.h>
 #include <string.h>
 
-int pid1, pid2, pid3, estat1, estat2, fd1[2], fd2[2];
+int pid1, pid2, estat1, estat2, fd1[2], fd2[2];
 
 /**
-* comanda: cat /proc/15735/status | grep Vm | sort -nrk2 >> sortida.txt
+* comandes:
+* ps -ef | grep u1987338
+* cat /proc/15735/status | grep Vm | sort -nrk2 >> sortida.txt
 **/
 int main(int argc, char *argv[]) {
 	char codiUdg[] = "u1987338";
@@ -22,6 +24,36 @@ int main(int argc, char *argv[]) {
 		strcat(ruta, "/status");
 		
 		// I segon, executa la comanda
+		estat1 = pipe(fd1);
+		if ((pid1 = fork()) == 0) {
+			estat2 = pipe(fd2);
+			if ((pid2 = fork()) == 0) { // codi del sort
+				close(0);
+				dup(fd2[0]);
+				close(fd2[0]);
+				close(fd2[1]);
+				execl("/bin/sort", "sort", "-nrk2", (char *)0);
+				exit(0);
+			} else { // codi del grep
+				close(0);
+				dup(fd1[0]);
+				close(fd1[0]);
+				close(fd1[1]);
+				close(1);
+				dup(fd2[1]);
+				close(fd2[0]);
+				close(fd2[1]);				
+				execl("/bin/grep", "grep", "Vm", (char *)0);
+				exit(0);
+			}
+		} else { // codi del cat
+			close(1);
+			dup(fd1[1]);
+			close(fd1[0]);
+			close(fd1[1]);
+			execl("/bin/cat", "cat", ruta, (char *)0);
+			exit(0);
+		}
 		
 		/*estat = pipe(fd);
 		pid = fork();
