3f02b78e85f9f2d8d516d565802d898bf7516c54
juanjogomezvillegas <juanjogomvil@gmail.com>
2024-04-10 17:51:56 +0200

    canvis a la pràctica 3, i al readme.md

--- P3/proc.c
+++ P3/proc.c
@@ -2,12 +2,35 @@
 // u1987338
 // Pràctica 3
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
 int sortida1, pid1, pid2, estat1, estat2, fd1[2], fd2[2];
+
+void redirect(char* fitxerOut, int canal, int mask) {
+	sortida1 = open(fitxerOut, O_WRONLY|O_CREAT|O_APPEND, mask);
+        close(canal);
+        dup(sortida1);
+        close(sortida1);
+}
+
+void netejaCanals() {
+	close(fd1[0]);
+        close(fd1[1]);
+        close(fd2[0]);
+        close(fd2[1]);
+}
+
+char* rutaProces(char* spid) {
+	char* ruta = "/proc/";
+        strcat(ruta, spid);
+        strcat(ruta, "/status");
+
+	return ruta;
+}
 
 /**
 * comandes:
@@ -18,11 +41,11 @@ int main(int argc, char *argv[]) {
 	char codiUdg[] = "u1987338";
 		
 	if (argc == 2) { // Validació de paràmetres
-		// Primer, construeix la ruta on anar a buscar l'estat del procés
+		//char* ruta = rutaProces(argv[1]);
 		char ruta[100] = "/proc/";
-		strcat(ruta, argv[1]);
-		strcat(ruta, "/status");
-		
+        	strcat(ruta, argv[1]);
+        	strcat(ruta, "/status");
+
 		// I segon, executa la comanda
 		estat1 = pipe(fd1);
 		estat2 = pipe(fd2);
@@ -32,13 +55,11 @@ int main(int argc, char *argv[]) {
 			close(1);
 			dup(fd1[0]);
 			dup(fd2[1]);
-			close(fd1[0]);
-			close(fd1[1]);
-			close(fd2[0]);
-			close(fd2[1]);
+			netejaCanals();
 			execl("/bin/grep", "grep", "Vm", (char *)0);
 			exit(0);
 		} else if (pid1 < 0) {
+			printf("\033[0;41mError en executar el fork \033[0m \n");
 			exit(1);
 		}
 		
@@ -46,30 +67,23 @@ int main(int argc, char *argv[]) {
 		if (pid2 == 0) { // codi del sort
 			close(0);
 			dup(fd2[0]);
-			close(fd1[0]);
-			close(fd1[1]);
-			close(fd2[0]);
-			close(fd2[1]);
-			//sortida1 = open("./sortida.txt", O_WRONLY|O_CREAT|O_TRUNC, 00600);
-			//close(1);
-			//dup(sortida1);
-			//close(sortida1);
+			netejaCanals();
+			redirect("./sortida.txt", 1, 0600);
 			execl("/bin/sort", "sort", "-nrk2", (char *)0);
 			exit(0);
 		} else if (pid2 < 0) {
+			printf("\033[0;41mError en executar el fork \033[0m \n");
 			exit(1);
 		}
 		
 		// codi del cat
 		close(1);
 		dup(fd1[1]);
-		close(fd1[0]);
-		close(fd1[1]);
-		close(fd2[0]);
-		close(fd2[1]);
+		netejaCanals();
 		execl("/bin/cat", "cat", ruta, (char *)0);
 		exit(0);
 	} else {
 		printf("\033[0;41mError amb els parametres. Ajuda:\n%s <pid del procés> \033[0m \n", argv[0]);
+		exit(1);
 	}
 }
--- README.md
+++ README.md
@@ -1,6 +1,64 @@
-Practiques de Sistemes Operatius a la UdG
+# Practiques de Sistemes Operatius a la UdG
 
-- P1: Programa amb shell que copia fitxers
-- P2: Programa amb shell que imita a la comanda find de unix
-- P3: Programa amb C
-- P4: Programa amb powershell
+## P1: Programa amb shell que copia fitxers
+
+Per executar-ho, abans cal fer.
+
+```sh
+$ cd P1; tar -xvf mesures; cd mesures
+```
+i una possible execució que cerca i copia tots els fitxers que es diguin *a*, *b* o *c*.
+
+```sh
+$ ./mostres a b c
+```
+
+[Per defecte si no se li passa cap paràmetre cerca i copia tots els fitxers]{style="color: blue"}
+
+## P2: Programa amb shell que imita a la comanda find de unix
+
+Els paràmetres d'aquest script són opcionals, si no es passa cap paràmetre mostra els fitxers del directori actual.
+
+```sh
+$ ./busca
+```
+
+I pot canviar de directori, i ordenar els fitxers per mida (per defecte s'ordenen de forma alfanùmerica).
+
+També pot redirigir la sortida estàndard cap a un fitxer.
+
+```sh
+$ ./busca -d ~ -o mida -s sortida.txt
+```
+
+I es poden afegir filtres per *inode*, *nom del fitxer* i *permisos*.
+
+```sh
+$ ./busca -d ~ -o mida -s sortida.txt -n "sortida" -i 20 -p -rw------- 
+```
+
+## P3: Programa amb C que mostra l'estat d'un procés
+
+Per executar l'script cal fer.
+
+```sh
+$ gcc -o proc proc.c
+$ ps -ef | grep <nom d'usuari de unix>
+```
+[El grep es per cercar els processos als que es tinguin permisos]{style="color: blue"}
+
+I un cop obtingut el pid del procés ja es pot fer.
+
+```sh
+$ ./proc <PID del procés>
+```
+
+L'script imita l'execució de la comanda.
+
+```sh
+$ cat /proc/<PID del procés>/status | grep Vm | sort -nrk2 >> sortida.txt
+```
+
+## P4: Programa amb C
+
+## P5: Programa amb Powershell
