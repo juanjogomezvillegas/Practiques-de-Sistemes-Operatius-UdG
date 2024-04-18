129604b77378bae0a88f92376d39202a0bb09a0d
juanjogomezvillegas <juanjogomvil@gmail.com>
2024-04-18 17:36:29 +0200

    inici de les practiques 4 i 5

---
+++ P4/encripta.c
@@ -0,0 +1,17 @@
+// Juan José Gómez Villegas
+// u1987338
+// Pràctica 4 - Encripta
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+/**
+* Pre:
+* Post:
+**/
+int main(int argc, char *argv[]) {
+	printf("Sóc l'encripta");
+}
---
+++ P5/mostres.ps1
@@ -0,0 +1,3 @@
+﻿cd mesures
+
+ls
