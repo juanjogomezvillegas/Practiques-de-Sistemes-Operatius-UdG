20a1c699b1324bb956f0d31a681a3def938f1f63
juanjogomezvillegas <juanjogomvil@gmail.com>
2024-04-26 19:33:53 +0200

    canvis a l'entrada de la Practica 4

--- P4/entrada.c
+++ P4/entrada.c
@@ -7,21 +7,36 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
+// Variables
 int i;
 char buff[30];
+char* FI = "XX";
 
 /**
-* Pre:
-* Post:
+* Pre: s1 i s2 no són buits
+* Post: retorna si s1 i s2 són iguals o no
+*/
+bool strIsEqual(char* s1, char* s2) {
+	return strcmp(s1, s2) == 0;
+}
+
+/**
+* Pre: Cert
+* Post: Va llegint paraules fins a entrar la cadena XX
 */
 int main(int argc, char *argv[]) {
-	i = read(0, buff, 30);
-	while (i > 0) {
-		write(1, buff, 30);
-		if (strcmp("XX", buff) == 0) {
-                        printf("Si");
-                }
-		i = read(0, buff, 30);
+	//i = read(0, buff, 30);
+	scanf("%s", buff);
+	while (!(strIsEqual(buff, FI))) {
+		//write(1, buff, 30);
+
+		//strcpy(buff, "");
+
+		printf("\n%s\n", buff);
+
+		//i = read(0, buff, 30);
+		scanf("%s", buff);
 	}
 }
