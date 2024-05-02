e1a7fe87bda3fb433817391ba1ff9bf5a7a37f1a
juanjogomezvillegas <juanjogomvil@gmail.com>
2024-05-02 17:08:21 +0200

    practica 4: entrada funcionant, falta encripta

--- P4/encripta.c
+++ P4/encripta.c
@@ -10,40 +10,85 @@
 #include <stdbool.h>
 
 // Variables
-int canal, i;
-char buff[30];
-char FI[] = "XX";
+int canal, i; // canal = pipe amb nom; i = caràcters llegits
+int pid1, pid2, pid3; // pids dels processos fills
+int fd[2], estat; // pipe interna
+char buff[30]; // paraula rebuda
 
 /**
-* Pre: s1 i s2 no són buits
-* Post: retorna si s1 i s2 són iguals
-*/
-bool strIsEqual(char s1[], char s2[]) {
-	return strcmp(s1, s2) == 0;
+* Pre: nom i mode no són buits
+* Post: Crea una pipe amb el nom indicat i l'obre també amb el mode indicat (lectura o escriptura)
+**/
+int crearNamedPipe(char* nom, mode_t mode) {
+	int aux = mknod(nom, S_IFIFO|0600, 0); // Crea la pipe amb nom
+	
+	if (aux != 0) { // I mostra si hi ha algun error
+		perror("Error al crear la pipe amb nom");
+		exit(1);
+	}
+	
+	aux = open(nom, mode, 0); // I si tot ha anat bé, obre la pipe amb nom amb el mode indicat
+	
+	if (aux < 0) { // I mostra si hi ha algun error
+		perror("Error al obrir la pipe amb nom");
+		exit(1);
+	}
+	
+	return aux;
 }
 
 /**
 * Pre:
 * Post:
 **/
-int main(int argc, char *argv[]) {
-	if (mkfifo("./canal", 00600) != 0) { // Primer, crea la pipe amb nom
-		perror("Error al crear la pipe amb nom");
+int main(int argc, char* argv[]) {
+	canal = crearNamedPipe("./canal", O_RDONLY); // Crea i obre la pipe amb nom en mode lectura
+	
+	estat = pipe(fd); // Crea la pipe interna
+	
+	pid1 = fork(); // Crea el procés de canvi de vocals
+	if (pid1 == 0) {
+		wait(2);
+		printf("Soc el 1");
+		wait(2);
+	} else if (pid1 < 0) {
+		perror("Error al crear el procés de canvi de vocals");
 		exit(1);
 	}
-	// I obre la pipe amb nom amb mode lectura
-	canal = open("./canal", O_RDONLY);
-	if (canal < 0) {
-		perror("Error al obrir la pipe amb nom");
+	
+	pid2 = fork(); // Crea el procés de cadena del revés
+	if (pid2 == 0) {
+		wait(2);
+		printf("Soc el 2");
+		wait(2);
+	} else if (pid1 < 0) {
+		perror("Error al crear el procés de cadena del revés");
 		exit(1);
 	}
-		
-	do {
-		i = read(canal, buff, 30);
+	
+	pid3 = fork(); // Crea el procés de duplica lletres
+	if (pid3 == 0) {
+		wait(2);
+		printf("Soc el 3");
+		wait(2);
+	} else if (pid1 < 0) {
+		perror("Error al crear el procés de duplica lletres");
+		exit(1);
+	}
+	
+	i = read(canal, buff, 30); // Rep la primera paraula
+	while (i > 0) {
+		// Tractar paraula
 		write(1, buff, i);
-	} while (!(strIsEqual(buff, FI)));
+		i = read(canal, buff, 30); // Rep la següent paraula
+	}
 
-	close(canal);
+	// Mata als processos fills
+	kill(pid1, 9);
+	kill(pid2, 9);
+	kill(pid3, 9);
+	close(estat); // Tanca la pipe interna
+	close(canal); // Tanca el canal de lectura a la pipe amb nom
 }
 
 // crear tres processos dormint
--- P4/entrada.c
+++ P4/entrada.c
@@ -12,7 +12,7 @@
 // Variables
 int canal, i;
 char buff[30];
-char FI[] = "XX";
+char FI[] = "XX\n";
 
 /**
 * Pre: s1 i s2 no són buits
@@ -27,16 +27,23 @@ bool strIsEqual(char s1[], char s2[]) {
 * Post: Va llegint paraules del teclat fins a entrar la cadena XX
 */
 int main(int argc, char *argv[]) {
-	canal = open("./canal", O_WRONLY);
-	if (canal < 0) {
+	// Obre la pipe amb nom
+	canal = open("./canal", O_WRONLY, 0);
+	if (canal < 0) { // I si no ha pogut obrir la pipe, mostra un missatge d'error
 		perror("Error al obrir la pipe amb nom");
 		exit(1);
 	}
 	
-	do {
-		i = read(0, buff, 30);
-		write(canal, buff, i);
-	} while (!(strIsEqual(buff, FI)));
+	// I si tot va bé, es posa a llegir paraules del teclat mentre la cadena entrada no sigui XX (la cadena XX no s'enviarà a l'encripta)
 	
-	close(canal);
+	printf("Entra paraules a encriptar (Per acabar entra \"XX\"):\n");
+	
+	i = read(0, buff, 30); // Llegeix la primera paraula
+	while (!(strIsEqual(buff, FI))) { // Mentre sigui diferent de XX
+		write(canal, buff, i); // Envia la paraula a l'encripta
+		i = read(0, buff, 30); // I llegeix la següent paraula
+		buff[i] = '\0'; // I abans de continuar, insereix una marca de fi de cadena a la paraula llegida per poder-la comparar
+	}
+	
+	close(canal); // Tanca el canal d'escriptura a la pipe amb nom
 }
