288df58e2d43dbb65971bcb7f4d0a26c8a7cbe8d
juanjogomezvillegas <juanjogomvil@gmail.com>
2024-05-14 20:06:13 +0200

    pràctica 5 acabada, falta repassar

--- P5/mesures/mostres.ps1
+++ P5/mesures/mostres.ps1
@@ -4,7 +4,8 @@
 # Post: Retorna la ruta des d'on executar l'script
 Function entrarMesures () {
     if ((Get-ChildItem -Path . | Where-Object -Property Name -EQ mesures).Length -eq 0) { # Si no troba cap directori mesures, l'script s'executarà des del directori actual
-        $ruta = "."
+        # En aquest cas, se suposa que l'script mostres estarà a dins del directori mesures (com a la pràctica 1)
+		$ruta = "."
     } else { # Si troba un directori mesures, l'script s'executarà amb la ruta mesures/
         $ruta = "mesures"
     }
@@ -39,16 +40,16 @@ $errors = 0
 $directoris = Directoris($ruta) # obté els directoris de la carpeta mostres
 
 if ($args.Length -eq 0) { # Si no s'ha entrat cap parametre
-    foreach ($i in $directoris) {
-        $fitxers = Fitxers($ruta + "/" + $i.Name)
+    foreach ($i in $directoris) { # Va iterant per cada directori
+        $fitxers = Fitxers($ruta + "/" + $i.Name) # Obté tots els fitxers de $i
 
-        foreach ($j in $fitxers) {
-            $origen = $ruta + "/" + $i.Name + "/" + $j.Name
+        foreach ($j in $fitxers) { # Va iterant per cada fitxer de $i, i fent una copia de $j amb el nom de $i
+			$origen = $ruta + "/" + $i.Name + "/" + $j.Name
             $desti = $j.Name.Substring(0, 1) + "_" + $i.Name + ".out"
             
             cp $origen $desti
 
-            $fcopiats = $fcopiats + 1
+            $fcopiats = $fcopiats + 1 # I incrementa el nombre de fitxers copiats
         }
     }
 } else { # Si s'ha entrat almenys un parametre
@@ -62,18 +63,19 @@ if ($args.Length -eq 0) { # Si no s'ha entrat cap parametre
         Write-Output "Error: Els parametres no son correctes"
         exit
     } else { # Si els parametres son correctes
-        foreach ($a in $args) {
-            foreach ($i in $directoris) {
-                $fitxers = Fitxers($ruta + "/" + $i.Name)
+        foreach ($a in $args) { # Per cada paramatre
+            foreach ($i in $directoris) { # Va iterant per cada directori
+                $fitxers = Fitxers($ruta + "/" + $i.Name) # Obté tots els fitxers de $i
 
-                foreach ($j in $fitxers) {
+                foreach ($j in $fitxers) { # Va iterant per cada fitxer de $i, i comprovant que $j es igual a $a (en minuscula)
                     if ($a.ToLower() -eq $j.Name.Substring(0, 1)) {
+						# Si coincideixen, copia $j amb el nom de $i
                         $origen = $ruta + "/" + $i.Name + "/" + $j.Name
                         $desti = $j.Name.Substring(0, 1) + "_" + $i.Name + ".out"
             
                         cp $origen $desti
 
-                        $fcopiats = $fcopiats + 1
+                        $fcopiats = $fcopiats + 1 # I incrementa el nombre de fitxers copiats
                     }
                 }
             }
